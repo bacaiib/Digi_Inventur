001a5b170396ddd12bffb8ade296995e973ce86a
bacaiib <roman-frommhold@gmx.net>
2025-12-18 14:56:28 +0100

    update 2.3

--- inventur/models.py
+++ inventur/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
 class Inventur(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=20, default="RUNNING")
+    stichtag = models.DateField(null=True, blank=True)
     created_at = models.DateField()
 
 class InventurPosition(models.Model):
-    CORTEXNR = models.IntegerField(primary_key=True)
-    HERST_NAME = models.CharField(max_length=200)
-    HERST_ART_NR = models.CharField(max_length=100)
-    ART_NAME = models.CharField(max_length=200)
-    VERPACKUNGSEINHEIT = models.FloatField()
-    ANZAHL = models.IntegerField()
-    EINHEIT_REST = models.FloatField()
-    ANZAHLZ = models.IntegerField()
+    inventur_id = models.ForeignKey(Inventur, on_delete=models.CASCADE)
+    Cortexnr = models.IntegerField(primary_key=True)
+    Hersteller = models.CharField(max_length=200)
+    Artikelnr_Hersteller = models.CharField(max_length=100)
+    Artikelnaam = models.CharField(max_length=200)
+    Verpackungseinheit = models.FloatField()
+    Anzahl = models.IntegerField()
+    Einheit_Rest = models.FloatField()
+    Anazahl = models.IntegerField()
     ART_ART_NR = models.CharField(max_length=100)
     EK = models.FloatField()
     WOG_NAME = models.CharField(max_length=50)
@@ -22,6 +25,6 @@ class InventurPosition(models.Model):
     WG_NR = models.IntegerField()
     EINHEIT = models.CharField(max_length=10)
     EINH_UMR = models.FloatField()
-    date = models.DateField()
+
 
 # Create your models here.
